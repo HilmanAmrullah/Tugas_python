698473e7f22c51916ddede7088e53abc1ad8356f
Hilman_Amr <hilmanamr22@gmail.com>
2022-10-24 20:43:44 +0700

    menambahkan file

---
+++ python.py
@@ -0,0 +1,24 @@
+nama = input("Masukkan nama kamu:")
+umur = input("Masukkan umur kamu:")
+
+print("Hello", nama,"umur kamu adalah",umur,"tahun")
+
+print("Latihan 1")
+
+a=input("Masukkan nilai A:")
+b=input("Masukkan nilai B:")
+
+print("Nilai variable A=",a)
+print("Nilai variable B=",b)
+total = int (a)+int(b)
+print("penjumlahan A+B=", total)
+
+print("MENGHITUNG LUAS & KELILING PERSEGI")
+
+s = float(input("\nMasukan Panjang Sisi: "))
+
+luas = s**2
+keliling = 4 * s
+
+print("\nLuas Persegi \t\t:",luas)
+print("Keliling Persegi\t:",keliling)
